9a305ca07f04b7b4ba00c4357910cd940912306c
kr-nikhilm <nikhil.m@knackroot.com>
2023-01-25 18:40:53 +0530

    end of day2

--- ArrayString/vector.cpp
+++ ArrayString/vector.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main()
 {
-    vector<int> v;
+    vector<int> v2;
     vector<int> v1(5);
-    vector<int> v2{2, 24, 12, 42, 1};
+    vector<int> v{2, 24, 12, 42, 1};
 
     cout << v.size() << "\n";
     v.resize(7);
     cout << v.size() << "\n";
+    cout<<v.capacity()<<"\n";
     sort(v.begin(), v.end());
     sort(v.begin(), v.end(),greater<int>());
+
+    v.push_back(56);
+    v.pop_back();
 
     return 0;
 }
